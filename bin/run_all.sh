676ded0588d6e4be7f6cc0ae12fd99fca3207f8e
Sławomir Garcarz <slawek.garcarz@email.com>
2023-06-18 20:47:32 +0200

    Added results for new datasets

--- bin/run_all.sh
+++ bin/run_all.sh
@@ -7,3 +7,7 @@ python run_experiment.py --dataset tafeng --model tifuknn --batch-size 1000 --nu
 python run_experiment.py --dataset dunnhumby --model tifuknn --batch-size 1000 --num_nearest_neighbors 900 --within_decay_rate 0.9 --group_decay_rate 0.6 --group_count 3 --alpha 0.2 --save_user_metrics True
 # valuedshoppers
 python run_experiment.py --dataset valuedshopper --model tifuknn --batch-size 1000 --num_nearest_neighbors 300 --within_decay_rate 1 --group_decay_rate 0.6 --group_count 7 --alpha 0.7 --save_user_metrics True
+# tmall
+python run_experiment.py --dataset tmall --model tifuknn --batch-size 1000 --num_nearest_neighbors 100 --within_decay_rate 0.6 --group_decay_rate 0.8 --group_count 18 --alpha 0.7 --save_user_metrics True
+# taobao
+python run_experiment.py --dataset taobao --model tifuknn --batch-size 1000 --num_nearest_neighbors 300 --within_decay_rate 0.6 --group_decay_rate 0.8 --group_count 10 --alpha 0.1 --save_user_metrics True
